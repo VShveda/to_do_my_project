a87bb2cc7e404826834b952358ed272270e7fa5d
Viktor Sveda <vinix.ru@gmail.com>
2024-07-24 11:04:58 +0300

    add index end views index

--- to_do/views.py
+++ to_do/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from to_do.models import Task
+
+
+def index(request):
+    tasks = Task.objects.all().order_by("-datetime", "complited")
+    return render(request, "index.html", {"tasks": tasks})
