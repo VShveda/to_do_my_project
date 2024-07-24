ce2385218d36d664fbce0d7af4055c210668b2ae
Viktor Sveda <vinix.ru@gmail.com>
2024-07-24 13:11:07 +0300

    update urls

--- to_do/urls.py
+++ to_do/urls.py
@@ -14,11 +14,11 @@ from to_do.views import (
 urlpatterns = [
     path("", index, name="index"),
     path("tags/", TagListView.as_view(), name="tag_list"),
-    path("tag_create/", TagCreateView.as_view(), name="tag_create"),
-    path("tag_update/<int:pk>/", TagUpdateView.as_view(), name="tag_update"),
-    path("tag_delete/<int:pk>/", TagDeleteView.as_view(), name="tag_delete"),
-    path("task_create/", TaskCreateView.as_view(), name="task_create"),
-    path("task_update/<int:pk>/", TaskUpdateView.as_view(), name="task_update"),
-    path("task_delete/<int:pk>/", TaskDeleteView.as_view(), name="task_delete"),
-    path("task_status/<int:pk>/", toggle_task_status, name="toggle_task_status"),
+    path("tag/create/", TagCreateView.as_view(), name="tag_create"),
+    path("tag/<int:pk>/update/", TagUpdateView.as_view(), name="tag_update"),
+    path("tag/<int:pk>/delete/", TagDeleteView.as_view(), name="tag_delete"),
+    path("task/create/", TaskCreateView.as_view(), name="task_create"),
+    path("task/<int:pk>/create/", TaskUpdateView.as_view(), name="task_update"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
+    path("task/<int:pk>/status/", toggle_task_status, name="toggle_task_status"),
 ]
