9fc4860ba284e547a4790dda1e756ca4d9706813
Viktor Sveda <vinix.ru@gmail.com>
2024-07-24 11:50:13 +0300

    add TagListView to url

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from to_do.views import index
+from to_do.views import index, TagListView
 
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tegs/", TagListView.as_view(), name="tag_list"),
 ]
