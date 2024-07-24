a4a576fad1f71241e783a3d820a9541ca3882add
Viktor Sveda <vinix.ru@gmail.com>
2024-07-24 12:27:47 +0300

    add create, update end delete tasks to url

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from to_do.views import index, TagListView, TagCreateView, TagUpdateView, TagDeleteView
+from to_do.views import index, TagListView, TagCreateView, TagUpdateView, TagDeleteView, TaskCreateView, TaskUpdateView, TaskDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -7,4 +7,7 @@ urlpatterns = [
     path("teg_create/", TagCreateView.as_view(), name="tag_create"),
     path("teg_update/<int:pk>/", TagUpdateView.as_view(), name="tag_update"),
     path("teg_delete/<int:pk>/", TagDeleteView.as_view(), name="tag_delete"),
+    path("task_create/", TaskCreateView.as_view(), name="task_create"),
+    path("task_update/<int:pk>/", TaskUpdateView.as_view(), name="task_update"),
+    path("task_delete/<int:pk>/", TaskDeleteView.as_view(), name="task_delete"),
 ]
