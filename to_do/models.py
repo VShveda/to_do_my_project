e89dd1e352b60caa5440d7182851d69a7d175d0c
Viktor Sveda <vinix.ru@gmail.com>
2024-07-24 10:38:41 +0300

    add Tag and Task models

--- to_do/models.py
+++ to_do/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.TextField()
+    datetime = models.DateTimeField(default=timezone.now)
+    deadline = models.DateField(null=True, blank=True)
+    complited = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    def __str__(self):
+        return self.content
