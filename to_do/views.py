22234a0131d0240bf659bdf7821ce711abee5864
Viktor Sveda <vinix.ru@gmail.com>
2024-07-24 13:06:59 +0300

    fix TagListView end index function

--- to_do/views.py
+++ to_do/views.py
@@ -7,13 +7,14 @@ from to_do.forms import TagForm, TaskForm
 
 
 def index(request):
-    tasks = Task.objects.all().order_by("-datetime", "complited")
+    tasks = Task.objects.all().order_by("complited", "-datetime")
     return render(request, "to_do/index.html", {"tasks": tasks})
 
 
 class TagListView(generic.ListView):
-    model = Task
+    model = Tag
     template_name = "to_do/tag_list.html"
+    context_object_name = "tags"
 
 
 class TagCreateView(generic.CreateView):
@@ -60,4 +61,4 @@ def toggle_task_status(request, pk):
     task = get_object_or_404(Task, id=pk)
     task.complited = not task.complited
     task.save()
-    return redirect('index')
+    return redirect("to_do:index")
