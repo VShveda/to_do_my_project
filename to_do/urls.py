d1511bfd4e7c2f13ec0b3bcffa31b9c785840805
Viktor Sveda <vinix.ru@gmail.com>
2024-07-24 12:01:55 +0300

    add Tag Create to url

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from to_do.views import index, TagListView
-
+from to_do.views import index, TagListView, TagCreateView
 
 urlpatterns = [
     path("", index, name="index"),
     path("tegs/", TagListView.as_view(), name="tag_list"),
+    path("teg_create/", TagCreateView.as_view(), name="tag_create"),
+
 ]
