761f8570c02bb9228f833ae2ef0556982ae34962
Viktor Sveda <vinix.ru@gmail.com>
2024-07-24 13:13:34 +0300

    fix urls task update

--- to_do/urls.py
+++ to_do/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path("tag/<int:pk>/update/", TagUpdateView.as_view(), name="tag_update"),
     path("tag/<int:pk>/delete/", TagDeleteView.as_view(), name="tag_delete"),
     path("task/create/", TaskCreateView.as_view(), name="task_create"),
-    path("task/<int:pk>/create/", TaskUpdateView.as_view(), name="task_update"),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task_update"),
     path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task_delete"),
     path("task/<int:pk>/status/", toggle_task_status, name="toggle_task_status"),
 ]
