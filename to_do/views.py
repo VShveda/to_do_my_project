f09184c7b44ed0460b0ed9cd91fc7a5b2593b168
Viktor Sveda <vinix.ru@gmail.com>
2024-07-24 11:48:49 +0300

    add TagListView

--- to_do/views.py
+++ to_do/views.py
@@ -8,3 +8,8 @@ def index(request):
     tasks = Task.objects.all().order_by("-datetime", "complited")
     return render(request, "to_do/index.html", {"tasks": tasks})
 
+
+class TagListView(generic.ListView):
+    model = Task
+    template_name = "to_do/tag_list.html"
+    context_object_name = "tegs"
