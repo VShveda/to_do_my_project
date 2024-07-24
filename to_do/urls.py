f701b20bdcdc4706999ff5735a09ed89121ebe0c
Viktor Sveda <vinix.ru@gmail.com>
2024-07-24 12:13:24 +0300

    add Update end delete to urls

--- to_do/urls.py
+++ to_do/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from to_do.views import index, TagListView, TagCreateView
+from to_do.views import index, TagListView, TagCreateView, TagUpdateView, TagDeleteView
 
 urlpatterns = [
     path("", index, name="index"),
     path("tegs/", TagListView.as_view(), name="tag_list"),
     path("teg_create/", TagCreateView.as_view(), name="tag_create"),
-
+    path("teg_update/<int:pk>/", TagUpdateView.as_view(), name="tag_update"),
+    path("teg_delete/<int:pk>/", TagDeleteView.as_view(), name="tag_delete"),
 ]
