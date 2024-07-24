0c39746d4d393ccd7a3a3d1137d699c01c63e642
Viktor Sveda <vinix.ru@gmail.com>
2024-07-24 13:04:52 +0300

    fix mistakes in url

--- to_do/urls.py
+++ to_do/urls.py
@@ -13,10 +13,10 @@ from to_do.views import (
 
 urlpatterns = [
     path("", index, name="index"),
-    path("tegs/", TagListView.as_view(), name="tag_list"),
-    path("teg_create/", TagCreateView.as_view(), name="tag_create"),
-    path("teg_update/<int:pk>/", TagUpdateView.as_view(), name="tag_update"),
-    path("teg_delete/<int:pk>/", TagDeleteView.as_view(), name="tag_delete"),
+    path("tags/", TagListView.as_view(), name="tag_list"),
+    path("tag_create/", TagCreateView.as_view(), name="tag_create"),
+    path("tag_update/<int:pk>/", TagUpdateView.as_view(), name="tag_update"),
+    path("tag_delete/<int:pk>/", TagDeleteView.as_view(), name="tag_delete"),
     path("task_create/", TaskCreateView.as_view(), name="task_create"),
     path("task_update/<int:pk>/", TaskUpdateView.as_view(), name="task_update"),
     path("task_delete/<int:pk>/", TaskDeleteView.as_view(), name="task_delete"),
